69cfabfae513878114154d56fe3b9c7986720e84
dde <danevans@acm.org>
2026-03-15 08:46:46 -0700

    CUDA header file initial design

---
+++ cuda_runtime.h
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#define cudaHostToDevice 1
+#define cudaDeviceToHost 1
+#define cudaDeviceToDevice 0
+struct threadIdx
+{
+    int x;
+    int y;
+};
+struct blockDim
+{
+    int x;  /* number of thread rows in a block */
+    int y;  /* number of threads in a block row */
+};
+struct blockIdx
+{
+    int x;
+    int y;
+};
+struct gridDim
+{
+    int x;  /* number of block rows in a grid */
+    int y;  /* number of blocks in a grid row */
+};
+void *memcpy(void *dev, void *hst, int len);
+void cudaMalloc(void **mem, int sz)
+{
+  void *bytes;
+  bytes = malloc(sz);
+  *mem = bytes;
+}
+void cudaFree(void *mem)
+{
+  free(mem);
+}
+void cudaMemcpy(void *dev, void *hst, int sz, int dir)
+{
+    memcpy(dev, hst, sz);
+}
